e2f862ca72029ddcf89f4d9c84c69c0f2d1f4210
ClaireBrts <claire.briantais@gmail.com>
2019-11-12 12:02:00 +0100

    type_activity training_activity

--- app/src/main/java/com/martin/promob/MainActivity.java
+++ app/src/main/java/com/martin/promob/MainActivity.java
@@ -2,7 +2,9 @@ package com.martin.promob;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String userLogin = intent.getStringExtra(LoginActivity.EXTRA_USERLOGIN);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("Hello "+ userLogin);
     }
+
+
+
 }
