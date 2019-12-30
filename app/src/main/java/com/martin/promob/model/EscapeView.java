313b832d5ea76b6bfdef73a37594421e46c22c76
ClaireBrts <claire.briantais@gmail.com>
2019-12-30 18:58:32 +0100

    foutu escape score fonctionne

--- app/src/main/java/com/martin/promob/model/EscapeView.java
+++ app/src/main/java/com/martin/promob/model/EscapeView.java
@@ -1,7 +1,8 @@
 package com.martin.promob.model;
-
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -16,6 +17,8 @@ import android.os.Build;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import androidx.appcompat.app.AlertDialog;
+
 import com.martin.promob.R;
 import com.martin.promob.ScoreActivity;
 import com.martin.promob.TypeActivity;
@@ -23,8 +26,8 @@ import com.martin.promob.TypeActivity;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
 public class EscapeView extends SurfaceView implements SurfaceHolder.Callback {
+
     private EscapeMissile missile;
     private EscapeBall ball;
     Context mContext;
@@ -33,17 +36,12 @@ public class EscapeView extends SurfaceView implements SurfaceHolder.Callback {
     private long tpstouch;
     private long chrono;
     private int mScreenX, mScreenY;
-
     private Bitmap bgImg;
-
     private int soundIdExplosion;
     private int soundIdBackground;
-
     private boolean soundPoolLoaded;
     private SoundPool soundPool;
     private static final int MAX_STREAMS = 100;
-
-
     private final List<Explosion> explosionList = new ArrayList<Explosion>();
 
 
@@ -52,25 +50,17 @@ public class EscapeView extends SurfaceView implements SurfaceHolder.Callback {
         getHolder().addCallback(this);
         this.mContext = context;
         escapeThread = new EscapeThread(this);
-
         life = 3;
         tpstouch = System.currentTimeMillis();
         chrono = 0;
         mScreenX = x;
         mScreenY = y;
-
         Bitmap bgSrc = BitmapFactory.decodeResource(context.getResources(), R.mipmap.starbackground);
         bgImg = Bitmap.createScaledBitmap(bgSrc, mScreenX, mScreenY, true);
-
-
         missile = new EscapeMissile(this.getContext());
         ball = new EscapeBall((SensorManager) mContext.getSystemService(Context.SENSOR_SERVICE), context);
-
-
         this.initSoundPool();
-
     }
-
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
         if (escapeThread.getState() == Thread.State.TERMINATED) {
@@ -79,13 +69,11 @@ public class EscapeView extends SurfaceView implements SurfaceHolder.Callback {
         escapeThread.setRunning(true);
         escapeThread.start();
     }
-
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
         missile.resize(width, height); // on définit la taille de la balle selon la taille de l'écran
         ball.resize(width, height);
     }
-
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
         boolean retry = true;
@@ -98,8 +86,6 @@ public class EscapeView extends SurfaceView implements SurfaceHolder.Callback {
             }
         }
     }
-
-
     public void update() {
         missile.moveWithCollisionDetection();
         ball.updateBall();
@@ -109,11 +95,9 @@ public class EscapeView extends SurfaceView implements SurfaceHolder.Callback {
             for (Explosion explosion : this.explosionList) {
                 explosion.update();
             }
-
             Iterator<Explosion> iterator = this.explosionList.iterator();
             while (iterator.hasNext()) {
                 Explosion explosion = iterator.next();
-
                 if (explosion.isFinish()) {
                     // If explosion finish, Remove the current element from the iterator & list.
                     iterator.remove();
@@ -123,18 +107,13 @@ public class EscapeView extends SurfaceView implements SurfaceHolder.Callback {
         }
         lose();
     }
-
-
     public static boolean isCollisionDetected(Bitmap bitmap1, int x1, int y1,
                                               Bitmap bitmap2, int x2, int y2) {
-
         if (bitmap1 == null || bitmap2 == null) {
             throw new IllegalArgumentException("bitmaps cannot be null");
         }
-
         Rect bounds1 = new Rect(x1, y1, x1 + bitmap1.getWidth(), y1 + bitmap1.getHeight());
         Rect bounds2 = new Rect(x2, y2, x2 + bitmap2.getWidth(), y2 + bitmap2.getHeight());
-
         if (Rect.intersects(bounds1, bounds2)) {
             Rect collisionBounds = getCollisionBounds(bounds1, bounds2);
             for (int i = collisionBounds.left; i < collisionBounds.right; i++) {
@@ -147,14 +126,11 @@ public class EscapeView extends SurfaceView implements SurfaceHolder.Callback {
                 }
             }
         }
-
         return false;
     }
-
     private static boolean isFilled(int pixel) {
         return pixel != Color.TRANSPARENT;
     }
-
     private static Rect getCollisionBounds(Rect rect1, Rect rect2) {
         int left = Math.max(rect1.left, rect2.left);
         int top = Math.max(rect1.top, rect2.top);
@@ -164,49 +140,44 @@ public class EscapeView extends SurfaceView implements SurfaceHolder.Callback {
     }
 
     public void lose() {
-        System.out.println("Valeur de life "+ life);
+
+        final Activity act = (Activity) this.getContext();
 
         if (life == 0) {
-            if (TypeActivity.compet) {
-                int score = (int) ((chrono / 30) / 10);
-                System.out.println("score du jeu escape " + score);
+
+            int score = (int) ((chrono / 30)/10);
+            System.out.println("valeur de life " + life);
+            escapeThread.setRunning(false);
+
+            if(TypeActivity.compet){
                 ScoreActivity.setmScoreJ1(score);
                 ScoreActivity.addmScoreTotJ1();
+                act.finish();
             }
-            final Activity act = (Activity) this.getContext();
-
-            act.finish();
-        }
-        else{
+            else{
+                act.finish();
+            }
 
         }
     }
-
     public void touch() {
         if (System.currentTimeMillis() - tpstouch >= 1500) {
             life -= 1;
-
             Bitmap boomSrc = BitmapFactory.decodeResource(this.getResources(), R.drawable.explosion);
             Explosion explosion = new Explosion(this, boomSrc, (int) ball.getxPos(), (int) ball.getyPos());
             this.explosionList.add(explosion);
-
             tpstouch = System.currentTimeMillis();
         }
-
     }
-
     private void initSoundPool() {
         // With Android API >= 21.
         if (Build.VERSION.SDK_INT >= 21) {
-
             AudioAttributes audioAttrib = new AudioAttributes.Builder()
                     .setUsage(AudioAttributes.USAGE_GAME)
                     .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
                     .build();
-
             SoundPool.Builder builder = new SoundPool.Builder();
             builder.setAudioAttributes(audioAttrib).setMaxStreams(MAX_STREAMS);
-
             this.soundPool = builder.build();
         }
         // With Android API < 21
@@ -214,27 +185,20 @@ public class EscapeView extends SurfaceView implements SurfaceHolder.Callback {
             // SoundPool(int maxStreams, int streamType, int srcQuality)
             this.soundPool = new SoundPool(MAX_STREAMS, AudioManager.STREAM_MUSIC, 0);
         }
-
         // When SoundPool load complete.
         this.soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
             @Override
             public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
                 soundPoolLoaded = true;
-
                 // Playing background sound.
                 playSoundBackground();
             }
         });
-
         // Load the sound background.mp3 into SoundPool
         // this.soundIdBackground= this.soundPool.load(this.getContext(), R.raw.background,1);
-
         // Load the sound explosion.wav into SoundPool
         this.soundIdExplosion = this.soundPool.load(this.getContext(), R.raw.explosion, 1);
-
-
     }
-
     public void playSoundExplosion() {
         if (this.soundPoolLoaded) {
             float leftVolumn = 0.8f;
@@ -243,7 +207,6 @@ public class EscapeView extends SurfaceView implements SurfaceHolder.Callback {
             int streamId = this.soundPool.play(this.soundIdExplosion, leftVolumn, rightVolumn, 1, 0, 1f);
         }
     }
-
     public void playSoundBackground() {
         if (this.soundPoolLoaded) {
             float leftVolumn = 0.8f;
@@ -252,7 +215,6 @@ public class EscapeView extends SurfaceView implements SurfaceHolder.Callback {
             int streamId = this.soundPool.play(this.soundIdBackground, leftVolumn, rightVolumn, 1, -1, 1f);
         }
     }
-
     @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
@@ -267,8 +229,5 @@ public class EscapeView extends SurfaceView implements SurfaceHolder.Callback {
         paint.setTextSize(50);
         canvas.drawText("Vie : " + life, (float) 100, (float) 100, paint);
         canvas.drawText("Temps : " + chrono / 30, (float) 500, (float) 100, paint);
-
-
     }
 }
-
